5d60a1b04d7248515d72d32d550197202b10774d
Michael van der Net <michaelvandernet@hotmail.com>
2016-12-12 13:32:04 +0100

    Added Blindwalls data

---
+++ MobileGuidingSystem/MobileGuidingSystem/Data/Blindwalls/BlindwallsDatabase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Dynamic;
+using Newtonsoft.Json;
+
+namespace MobileGuidingSystem.Data.Blindwalls
+{
+    class BlindwallsDatabase
+    {
+        public static BlindwallsDatabase Instance => _instance ?? (_instance = new BlindwallsDatabase());
+
+        private static BlindwallsDatabase _instance;
+
+        public string status { get; set; }
+        public int count { get; set; }
+        public int count_total { get; set; }
+        public int pages { get; set; }
+        public List<Post> posts { get; set; }
+        public Query query { get; set; }
+
+        
+    }
+
+    
+}
