8b7cad5e8bb777787a965da0986746b5ba55b808
casdeGroot <casdegroot@live.nl>
2016-12-15 20:04:43 +0100

    added sights and route generation not working perfectly tho

--- MobileGuidingSystem/MobileGuidingSystem/Data/Sight.cs
+++ MobileGuidingSystem/MobileGuidingSystem/Data/Sight.cs
@@ -1,7 +1,25 @@
-﻿namespace MobileGuidingSystem.Data
+﻿using System;
+using Windows.Devices.Geolocation;
+using Windows.Foundation;
+using Windows.Storage.Streams;
+
+namespace MobileGuidingSystem.Data
 {
     public class Sight
     {
-        
+        public string IconPath;
+        public string Name;
+        public Geopoint Position;
+        public RandomAccessStreamReference Image;
+        public Point NormalizedAnchorPoint;
+
+        public Sight(string name, string iconPath, Geopoint position)
+        {
+            this.Name = name;
+            this.IconPath = iconPath;
+            this.Position = position;
+            Image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/" + iconPath));
+            NormalizedAnchorPoint = new Point(1,1);
+        }
     }
 }
--- MobileGuidingSystem/MobileGuidingSystem/View/MainPage.xaml.cs
+++ MobileGuidingSystem/MobileGuidingSystem/View/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using MobileGuidingSystem.Data;
 using MobileGuidingSystem.ViewModel;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
@@ -25,11 +26,24 @@ namespace MobileGuidingSystem
     public sealed partial class MainPage : Page
     {
         public MainModel model;
+
         public MainPage()
         {
             this.InitializeComponent();
             model = new MainModel(MyMap);
+            DataContext = model;
         }
 
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var b = (StackPanel)sender;
+            var selected = (Sight)b.DataContext;
+            MyMap.Center = selected.Position;
+
+            Popup popup = new Popup();
+            popup.DataContext = selected;
+            
+            
+        }
     }
 }
