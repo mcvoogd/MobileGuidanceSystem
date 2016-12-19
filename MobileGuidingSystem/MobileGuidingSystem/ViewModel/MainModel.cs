1e89423603d0bbeebf2617ece149054c755644ce
cezan123 <cezan@live.nl>
2016-12-19 16:30:32 +0100

    sights are mapsIcons now and route is drawn

--- MobileGuidingSystem/MobileGuidingSystem/ViewModel/MainModel.cs
+++ MobileGuidingSystem/MobileGuidingSystem/ViewModel/MainModel.cs
@@ -27,31 +27,40 @@ namespace MobileGuidingSystem.ViewModel
 
         private readonly Color _routeColor = Colors.Blue;
         private readonly Color _outlineColor = Colors.LightBlue;
+        public IRandomAccessStreamReference iconImage { get; set;}
+        public Point Anchor { get; set; }
+        
 
         public MainModel(MapControl mapcontrol)
         {
             _map = mapcontrol;
             Sights = new ObservableCollection<ISight>();
             LoadData();
-            DrawRoutes(Sights);
-            MyLocation = new Geopoint(new BasicGeoposition() {Latitude = 51.5860591, Longitude = 4.793500600000016});
+            MyLocation = new Geopoint(new BasicGeoposition() { Latitude = 51.5860591, Longitude = 4.793500600000016 });
             User = new User();
+            DrawRoutes(BlindwallsDatabase.Sights);
+            iconImage = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/home-pin.png"));
+            Anchor = new Point(0.5, 1);
             //drawRoute(new Geopoint(new BasicGeoposition() { Latitude = 51.59000, Longitude = 4.781000 }), new Geopoint(new BasicGeoposition(){ Longitude = 4.780172, Latitude = 51.586267}) );
-            _map.ZoomLevelChanged += _map_ZoomLevelChanged;
+            //  _map.ZoomLevelChanged += _map_ZoomLevelChanged;
         }
 
         private void LoadData()
         {
-            BlindwallsDatabase.Sights.ForEach(s=>Sights.Add(s));
-        }
-
-        private void _map_ZoomLevelChanged(MapControl sender, object args)
-        {
-            if (_map.ZoomLevel < 17)
+            //BlindwallsDatabase.Sights.ForEach(s=>Sights.Add(s));
+            foreach (ISight sight in BlindwallsDatabase.Sights)
             {
-                _map.ZoomLevel = 17;
+                drawSight(sight.Position, sight.Name);
             }
         }
+
+        //private void _map_ZoomLevelChanged(MapControl sender, object args)
+        //{
+        //    if (_map.ZoomLevel < 17)
+        //    {
+        //        _map.ZoomLevel = 17;
+        //    }
+        //}
 
         public async void DrawRoute(Geopoint p1, Geopoint p2)
         {
@@ -77,23 +86,47 @@ namespace MobileGuidingSystem.ViewModel
                 routeView.OutlineColor = _outlineColor;
                 _map.Routes.Add(routeView);
             }
+            else if (routeFinderResult.Status == MapRouteFinderStatus.NoRouteFoundWithGivenOptions)
+            {
+                MapRouteView routeView = new MapRouteView(routeFinderResult.Route);
+                routeView.RouteColor = _routeColor;
+                routeView.OutlineColor = _outlineColor;
+                _map.Routes.Add(routeView);
+            }
         }
 
-        public void DrawRoutes(ObservableCollection<ISight> sightlist)
+        public void DrawRoutes(List<ISight> sightlist)
         {
             List<Geopoint> positions = new List<Geopoint>();
+
 
             foreach (ISight s in sightlist)
             {
                 positions.Add(s.Position);
             }
-
             DrawRoutes(positions);
         }
 
         public override void NextPage(object user)
         {
             throw new NotImplementedException();
+        }
+
+        public void drawSight(Geopoint position, String title)
+        {
+            var ancherpoint = new Point(0.5,1);
+            var image = RandomAccessStreamReference.CreateFromUri(new Uri("ms-appx:///Assets/home-pin.png"));
+            var Sight = new MapIcon
+            {
+                Title = title,
+                Location = position,
+                NormalizedAnchorPoint = ancherpoint,
+                Image = image,
+                ZIndex = 4,
+                CollisionBehaviorDesired = MapElementCollisionBehavior.RemainVisible
+            
+            };
+            _map.MapElements.Add(Sight);
         }
 
         public void DrawPlayer(Geoposition position)
@@ -109,6 +142,7 @@ namespace MobileGuidingSystem.ViewModel
                 Image = image,
                 ZIndex = 13
             };
+            User.Location = player.Location;
             UpdatePos();
             _map.MapElements.Add(player);
         }
