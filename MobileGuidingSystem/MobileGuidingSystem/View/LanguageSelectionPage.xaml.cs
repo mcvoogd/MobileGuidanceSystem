04c83b103c44aa3c0456873d2e4b793efb97b686
Martijn <mcvoogd@avans.nl>
2016-12-22 14:12:23 +0100

    add language change function

--- MobileGuidingSystem/MobileGuidingSystem/View/LanguageSelectionPage.xaml.cs
+++ MobileGuidingSystem/MobileGuidingSystem/View/LanguageSelectionPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Windows.Globalization;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -12,6 +16,22 @@ namespace MobileGuidingSystem.View
         public LanguageSelectionPage()
         {
             this.InitializeComponent();
+        }
+
+        private void Dutch_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            ApplicationLanguages.PrimaryLanguageOverride = "nl-NL";
+            Windows.ApplicationModel.Resources.Core.ResourceContext.GetForViewIndependentUse().Reset();
+            Windows.ApplicationModel.Resources.Core.ResourceManager.Current.DefaultContext.Reset();
+            Frame.Navigate(this.GetType());
+        }
+
+        private void English_OnTapped(object sender, TappedRoutedEventArgs e)
+        {
+            ApplicationLanguages.PrimaryLanguageOverride = "en-US";
+            Windows.ApplicationModel.Resources.Core.ResourceContext.GetForViewIndependentUse().Reset();
+            Windows.ApplicationModel.Resources.Core.ResourceManager.Current.DefaultContext.Reset();
+            Frame.Navigate(this.GetType());
         }
     }
 }
